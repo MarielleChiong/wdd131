568a02fee62463c245c6311ba2addf56bcd433fe
MarielleChiong <chi22003@byui.edu>
2025-09-13 17:01:20 +0800

    updated and fixed the lastModified part of the footer

--- scripts/temples.js
+++ scripts/temples.js
@@ -22,3 +22,9 @@ window.addEventListener('resize', () => {
 // Set the current year in the footer
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
+
+// Set the last modified date
+const lastmod = document.querySelector('#lastModified');
+if (lastmod) {
+    lastmod.textContent = document.lastModified;
+}
